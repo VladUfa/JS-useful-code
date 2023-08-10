4713b9f95a8ec80b8f03024bb5e90e5e3d33d34d
Vladimir Prokaev <104609786+VladUfa@users.noreply.github.com>
2023-08-10 12:52:16 -0700

    Update examples

--- Holidays/index.js
+++ Holidays/index.js
@@ -4,7 +4,9 @@ const buisnessDays = new holidaysModule.BuisnessDays
 
 
 //console.log(buisnessDays.itIsBuisnessDay(new Date()))
-const today = new Date(2022, 5, 19)
-console.log(buisnessDays.getHolidays())
+const today = new Date()
+console.log(today)
 console.log(buisnessDays.itIsBuisnessDay(today))
 console.log(buisnessDays.itIsHoliday(today))
+console.log(buisnessDays.addBuisnessDays(today, -5))
+console.log(buisnessDays.getHolidays())
